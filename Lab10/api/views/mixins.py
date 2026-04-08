c75b5e0f5c4d9d38a91a8081979c27528c2b65ad
Vladislav Lizko <vladlzk26@gmail.com>
2026-04-08 15:09:29 +0500

    Lab10

---
+++ Lab10/api/views/mixins.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
+from api.models import Product
+from api.serializers import ProductSerializer
+
